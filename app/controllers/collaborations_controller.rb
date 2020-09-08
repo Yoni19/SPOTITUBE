2ebd5be17024e0a2677f81f7b13ab98bc52619cc
Orane31 <orane.chaper@gmail.com>
2020-09-08 22:38:05 +0200

    controller collaborations created

---
+++ app/controllers/collaborations_controller.rb
@@ -0,0 +1,24 @@
+class CollaborationsController < ApplicationController
+
+    def index
+        @collaborations = Collaboration.all
+        @playlist = Playlist.find(params[:playlist_id])
+    end
+
+    def new
+        @playlist = Playlist.find(params[:playlist_id])
+    end
+    
+    def create
+      @playlist = Playlist.find(params[:playlist_id])
+      @collaboration = Collaboration.new(playlist: Playlist.find(params[:playlist_id]), collaborator: User.find(params[:playlist_id])
+      if @collaboration.save
+        flash[:success] = "Merci #{current_user.name} ! #{@collaboration.collaborator.name} peut maintenant participer à la playlist !"
+        redirect_to :controller => 'playlists', :action => 'show' , id: Playlist.find(params[:playlist_id])
+      else
+        flash[:danger] = "Nous n'avons pas réussi à ajouter de collaborateur pour la (ou les) raison(s) suivante(s) : #{@collaboration.errors.full_messages.each {|message| message}.join('')}"
+        render :action => 'new'
+      end
+    end
+
+end
