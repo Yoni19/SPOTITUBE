b4faac56045d432ab2ee7c6bd00a820e085293a1
Yoni19 <anthony.krief@hotmail.fr>
2020-09-08 14:35:20 +0200

    Add seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+
+User.destroy_all
+
+Playlist.destroy_all
+
+
+
+5.times do |i|
+
+
+    User = User.create(email: Faker::Name.first_name + "@test.com", 
+
+    pseudo: Faker::Name.first_name,, 
+
+    first_name: Faker::Name.first_name,
+
+    last_name: Faker::Name.last_name,
+
+    description: Faker::Lorem.paragraph,
+
+
+    password: "123456",
+
+    password_confirmation: "123456",
+
+    is_admin: false
+
+  )
+    end
+
+puts "Create User : Done"
+
+
+
+
+
+
+
+5.times do |x|
+
+    playlist = Playlist.create(
+
+      title: Faker::ChuckNorris.fact,
+      description: Faker::Lorem.paragraph
+    
+    )
+
+puts "Create Playlist - Done"
+  
+
+
+
+
+end
+
