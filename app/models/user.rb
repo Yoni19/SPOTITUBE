6440f71228fea7ca35e615a6dff739df5e1d8da2
Yoni19 <anthony.krief@hotmail.fr>
2020-09-08 16:17:58 +0200

    Model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+has_many :collaborations
+has_many :playlist, foreign_key: 'owner_id', class: "Playlist"
 end
