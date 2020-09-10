9b7758423c5e6bd9dd1db747a94c9cf2382ce74e
Orane31 <orane.chaper@gmail.com>
2020-09-10 12:38:21 +0200

    videoplaylist controller part done

--- app/controllers/video_playlists_controller.rb
+++ app/controllers/video_playlists_controller.rb
@@ -5,14 +5,14 @@
   def index
     @playlist = Playlist.find(params[:playlist_id])
     @video_playlists = VideoPlaylist.al
-    $ l  end
+  end
 
   def new
     create
   end
 
   def create
-    @video_playlist= Attendance.new(participant_id: current_user.id, stripe_customer_id: params[:token], event: Event.find(params[:event]))
+    @video_playlist = VideoPlaylist.new(video_id: @video.id, playlist_id: @playlist.id)
 
    
   end
