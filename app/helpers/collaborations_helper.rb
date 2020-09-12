58e50a67844c8ec8191f67d731d622adfc047dcb
Orane31 <orane.chaper@gmail.com>
2020-09-12 17:20:12 +0200

    conditions collabroators and owners implemented on view playlists

--- app/helpers/collaborations_helper.rb
+++ app/helpers/collaborations_helper.rb
@@ -2,8 +2,6 @@ module CollaborationsHelper
 
       def collaborates?(playlist)
         Collaboration.where(collaborator_id: current_user.id, playlist_id: playlist.id).size > 0
-
-
       end
 end
 
